41d6bacb3a596c4563151f93a58491a1a6dd6004
Téo Sanchez <teo.sanchez@inria.fr>
2023-03-16 18:27:40 +0100

    terser install

--- _plugins/terser.rb
+++ _plugins/terser.rb
@@ -1,7 +1,7 @@
 require 'terser'
 require 'sprockets'
 
-Sprockets.register_transformer 'application/javascript', 'application/javascript', Terser
+Sprockets.register_compressor 'application/javascript', 'application/javascript', Terser
 Sprockets.register_mime_type 'application/javascript', extensions: ['.js'], charset: :unicode
 
 Sprockets.register_bundle_processor 'application/javascript', Terser::BundleProcessor
