0df836586ecbaee853eb776ce24356e1025d6e27
Téo Sanchez <teo.eoures@gmail.com>
2023-11-05 16:01:56 +0100

    extend dataset

--- assets/demos_data/transform.py
+++ assets/demos_data/transform.py
@@ -2,9 +2,9 @@ import os
 import pdb
 stop = pdb.set_trace
 
-for dir in ["miniRETINA/sain/", "miniRETINA/DR/", "miniRETINA/ARMD/"]:
+for dir in ["miniSKIN/train/", "miniSKIN/test/"]:
     for f in os.listdir(dir):
-        if "png" in f:
+        if "jpg" in f:
             # os.system("ffmpeg -i {0} -preset ultrafast banana_transformed_{1}.jpg".format(f, f.split("_")[1].split(".")[0]))
             os.system("""ffmpeg -i {0} -vf "crop=ih:ih" {1}""".format(dir+f, dir+f.split(".")[0]+"_transformed.png"))
             os.system("ffmpeg -i {0} -vf scale=224:224 {1}".format(dir+f.split(".")[0]+"_transformed.png", dir+f.split(".")[0]+"_final.png"))
